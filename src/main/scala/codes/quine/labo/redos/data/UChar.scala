aaaeb78f526876dc3cf49aa3e6393f5a1f5ed4ea
TSUYUSATO Kitsune <make.just.on@gmail.com>
2020-10-16 00:46:23 +0900

    fix: UChar#toString returns human readable format

--- src/main/scala/codes/quine/labo/redos/data/UChar.scala
+++ src/main/scala/codes/quine/labo/redos/data/UChar.scala
@@ -1,19 +1,34 @@
 package codes.quine.labo.redos.data
 
+import com.ibm.icu.lang.UCharacter
+
 /** UChar is a Unicode code point. */
 final case class UChar(value: Int) extends Ordered[UChar] {
 
   /** Compares to other Unicode code point. */
   def compare(that: UChar): Int = this.value.compare(that.value)
 
-  /** Converts to a UTF-16 representation. */
-  override def toString: String =
-    if (value <= 0xffff) String.valueOf(Array(value.toChar))
+  /** Converts to a UTF-16 characters. */
+  def toChars: Array[Char] =
+    if (value <= 0xffff) Array(value.toChar)
     else {
       val c1 = (value >> 16) & 0xffff
       val c2 = value & 0xffff
       val d1 = (0xd800 | ((c1 - 1) << 6) | (c2 >> 10)).toChar
       val d2 = (0xdc00 | (c2 & 0x3ff)).toChar
-      String.valueOf(Array(d1, d2))
+      Array(d1, d2)
     }
+
+  /** Show this Unicode point as human readable format. */
+  override def toString: String = value match {
+    case 0x09                           => "\\t"
+    case 0x0a                           => "\\n"
+    case 0x0b                           => "\\v"
+    case 0x0c                           => "\\f"
+    case 0x0d                           => "\\r"
+    case c if UCharacter.isPrintable(c) => String.valueOf(toChars)
+    case c if c < 0x100                 => f"\\x$c%02X"
+    case c if c < 0x10000               => f"\\u$c%04X"
+    case c                              => f"\\u{$c%X}"
+  }
 }
--- src/test/scala/codes/quine/labo/redos/data/UCharSuite.scala
+++ src/test/scala/codes/quine/labo/redos/data/UCharSuite.scala
@@ -9,8 +9,20 @@ object UCharSuite extends SimpleTestSuite {
     assertEquals(UChar(0x42).compare(UChar(0x41)), 1)
   }
 
+  test("UChar#toChars") {
+    assertEquals(String.valueOf(UChar(0x41).toChars), "A")
+    assertEquals(String.valueOf(UChar(0x1f363).toChars), "🍣")
+  }
+
   test("UChar#toString") {
-    assertEquals(UChar(0x41).toString, "A")
-    assertEquals(UChar(0x1f363).toString, "🍣")
+    assertEquals(UChar(0x09).toString, "\\t")
+    assertEquals(UChar(0x0a).toString, "\\n")
+    assertEquals(UChar(0x0b).toString, "\\v")
+    assertEquals(UChar(0x0c).toString, "\\f")
+    assertEquals(UChar(0x0d).toString, "\\r")
+    assertEquals(UChar('a').toString, "a")
+    assertEquals(UChar(0).toString, "\\x00")
+    assertEquals(UChar(0xffff).toString, "\\uFFFF")
+    assertEquals(UChar(0x10ffff).toString, "\\u{10FFFF}")
   }
 }
