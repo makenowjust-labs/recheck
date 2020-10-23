5b435e2af37d754a1e9c481008ddb4a502e56079
TSUYUSATO Kitsune <make.just.on@gmail.com>
2020-10-24 01:08:22 +0900

    feat: add Graph

---
+++ src/main/scala/codes/quine/labo/redos/data/Graph.scala
@@ -0,0 +1,135 @@
+package codes.quine.labo.redos.data
+
+import scala.collection.mutable
+
+/** Graph utilities. */
+object Graph {
+
+  /** Creates a graph from the list of edges. */
+  def from[V, L](edges: Seq[(V, L, V)]): Graph[V, L] =
+    Graph(edges.groupMap(_._1)(vlv => (vlv._2, vlv._3)).withDefaultValue(Seq.empty))
+}
+
+/** Graph is a directed labelled multi-graph implementation. */
+final case class Graph[V, L] private (neighbors: Map[V, Seq[(L, V)]]) {
+
+  /** Lists edges of this graph. */
+  def edges: Seq[(V, L, V)] =
+    neighbors.flatMap { case v1 -> lvs => lvs.map { case (l, v2) => (v1, l, v2) } }.toSeq
+
+  /** Lists vertices of this graph. */
+  def vertices: Set[V] = neighbors.keySet | neighbors.values.flatMap(_.map(_._2)).toSet
+
+  /** Computes a reversed graph of this graph. */
+  def reverse: Graph[V, L] =
+    Graph.from(edges.map { case (v1, l, v2) => (v2, l, v1) })
+
+  /** Computes a strongly connected components of this graph.
+    *
+    * This method uses [[https://en.wikipedia.org/wiki/Tarjan's_strongly_connected_components_algorithm Tarjan's algorithm]].
+    */
+  def scc: Seq[Seq[V]] = {
+    var clock = 0
+    val visited = mutable.Map.empty[V, Int]
+    val lowlinks = mutable.Map.empty[V, Int]
+
+    val stack = mutable.Stack.empty[V]
+    val inStack = mutable.Set.empty[V]
+
+    val components = Seq.newBuilder[Seq[V]]
+
+    def dfs(v1: V): Unit = {
+      visited(v1) = clock
+      lowlinks(v1) = clock
+      clock += 1
+
+      stack.push(v1)
+      inStack.add(v1)
+
+      for ((_, v2) <- neighbors.getOrElse(v1, Seq.empty)) {
+        if (!visited.contains(v2)) {
+          dfs(v2)
+          lowlinks(v1) = Math.min(lowlinks(v1), lowlinks(v2))
+        } else if (inStack.contains(v2)) {
+          lowlinks(v1) = Math.min(lowlinks(v1), visited(v2))
+        }
+      }
+
+      if (lowlinks(v1) == visited(v1)) {
+        val component = Seq.newBuilder[V]
+        var v2 = stack.pop()
+        inStack.remove(v2)
+        while (v1 != v2) {
+          component.addOne(v2)
+          v2 = stack.pop()
+          inStack.remove(v2)
+        }
+        component.addOne(v1)
+        components.addOne(component.result())
+      }
+    }
+
+    for (v <- vertices) {
+      if (!visited.contains(v)) {
+        dfs(v)
+      }
+    }
+
+    components.result()
+  }
+
+  /** Computes a path from the sources to the target. */
+  def path(sources: Set[V], target: V): Option[Seq[L]] = {
+    val queue = mutable.Queue.empty[(V, Seq[L])]
+    val visited = mutable.Set.empty[V]
+
+    queue.enqueueAll(sources.map((_, Seq.empty)))
+    visited.addAll(sources)
+
+    while (queue.nonEmpty) {
+      val (v1, path) = queue.dequeue()
+      if (v1 == target) {
+        return Some(path)
+      }
+      for ((l, v2) <- neighbors(v1); if !visited.contains(v2)) {
+        queue.enqueue((v2, path :+ l))
+        visited.add(v2)
+      }
+    }
+
+    return None
+  }
+
+  /** Computes a new graph collects vertices and edges can be reachable from the init vertices. */
+  def reachable(init: Set[V]): Graph[V, L] = {
+    val queue = mutable.Queue.empty[V]
+    val reachable = mutable.Set.empty[V]
+    val newEdges = Map.newBuilder[V, Seq[(L, V)]]
+
+    queue.enqueueAll(init)
+    reachable.addAll(init)
+
+    while (queue.nonEmpty) {
+      val v1 = queue.dequeue()
+      val es = neighbors(v1)
+      val vs = es.map(_._2)
+      if (es.nonEmpty) newEdges.addOne(v1 -> es)
+      queue.enqueueAll(vs.filterNot(reachable.contains(_)))
+      reachable.addAll(vs)
+    }
+
+    Graph(newEdges.result())
+  }
+
+  /** Computes a map from a vertex to vertices being reachable from the vertex.
+    *
+    * Note that it causes a stack overflow when this graph has a cycle.
+    */
+  def reachableMap: Map[V, Set[V]] = {
+    val map = mutable.Map.empty[V, Set[V]]
+    def dfs(v1: V): Set[V] =
+      map.getOrElseUpdate(v1, Set(v1) ++ neighbors(v1).flatMap { case (_, v) => dfs(v) }.toSet)
+    vertices.foreach(dfs(_))
+    map.toMap
+  }
+}
---
+++ src/test/scala/codes/quine/labo/redos/data/GraphSuite.scala
@@ -0,0 +1,93 @@
+package codes.quine.labo.redos.data
+
+class GraphSuite extends munit.FunSuite {
+  test("Graph.from") {
+    val g = Graph.from(Seq((1, 'a', 2), (1, 'a', 3), (2, 'b', 3)))
+    assertEquals(g.neighbors, Map(1 -> Seq(('a', 2), ('a', 3)), 2 -> Seq(('b', 3))))
+  }
+
+  test("Graph#edges") {
+    val g = Graph.from(Seq((1, 'a', 2), (1, 'a', 3), (2, 'b', 3)))
+    assertEquals(g.edges, Seq((1, 'a', 2), (1, 'a', 3), (2, 'b', 3)))
+  }
+
+  test("Graph#vertices") {
+    val g = Graph.from(Seq((1, 'a', 2), (1, 'a', 3), (2, 'b', 3)))
+    assertEquals(g.vertices, Set(1, 2, 3))
+  }
+
+  test("Graph#reverse") {
+    val g1 = Graph.from(Seq((1, 'a', 2), (1, 'a', 3), (2, 'b', 3)))
+    val g2 = Graph.from(Seq((2, 'a', 1), (3, 'a', 1), (3, 'b', 2)))
+    assertEquals(g1.reverse, g2)
+  }
+
+  test("Graph#scc") {
+    val g = Graph.from(
+      Seq(
+        (1, (), 2),
+        (2, (), 3),
+        (3, (), 4),
+        (4, (), 2),
+        (3, (), 5),
+        (5, (), 6),
+        (6, (), 5),
+        (6, (), 7)
+      )
+    )
+    assertEquals(g.scc, Seq(Seq(7), Seq(6, 5), Seq(4, 3, 2), Seq(1)))
+  }
+
+  test("Graph#path") {
+    val g = Graph.from(
+      Seq(
+        (1, 'a', 2),
+        (2, 'b', 3),
+        (3, 'c', 2),
+        (3, 'd', 4),
+        (3, 'e', 5)
+      )
+    )
+    assertEquals(g.path(Set(1), 4), Some(Seq('a', 'b', 'd')))
+    assertEquals(g.path(Set(1), 5), Some(Seq('a', 'b', 'e')))
+    assertEquals(g.path(Set(4), 5), None)
+    assertEquals(g.path(Set(4, 3), 5), Some(Seq('e')))
+  }
+
+  test("Graph#reachable") {
+    val g = Graph.from(
+      Seq(
+        (1, (), 2),
+        (2, (), 3),
+        (3, (), 1),
+        (4, (), 5),
+        (6, (), 7)
+      )
+    )
+    assertEquals(g.reachable(Set(1)), Graph.from(Seq((1, (), 2), (2, (), 3), (3, (), 1))))
+    assertEquals(g.reachable(Set(4, 6)), Graph.from(Seq((4, (), 5), (6, (), 7))))
+  }
+
+  test("Graph#reachableMap") {
+    val g = Graph.from(
+      Seq(
+        (1, (), 2),
+        (2, (), 3),
+        (4, (), 5),
+        (6, (), 7)
+      )
+    )
+    assertEquals(
+      g.reachableMap,
+      Map(
+        1 -> Set(1, 2, 3),
+        2 -> Set(2, 3),
+        3 -> Set(3),
+        4 -> Set(4, 5),
+        5 -> Set(5),
+        6 -> Set(6, 7),
+        7 -> Set(7)
+      )
+    )
+  }
+}
