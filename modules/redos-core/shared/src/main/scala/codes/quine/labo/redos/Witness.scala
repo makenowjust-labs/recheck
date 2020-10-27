ea31a15f963cfcdf450473825a139ff75c82de4a
TSUYUSATO Kitsune <make.just.on@gmail.com>
2020-10-28 00:14:13 +0900

    feat: add Witness methods

--- modules/redos-core/shared/src/main/scala/codes/quine/labo/redos/Witness.scala
+++ modules/redos-core/shared/src/main/scala/codes/quine/labo/redos/Witness.scala
@@ -5,4 +5,18 @@ package codes.quine.labo.redos
   * For example, when a witness object forms `Witness(Seq((x, y), (z, w)), u)`,
   * an actual witness string is `x y^n z w^n u` for any integer `n`.
   */
-final case class Witness[A](pumps: Seq[(Seq[A], Seq[A])], suffix: Seq[A])
+final case class Witness[A](pumps: Seq[(Seq[A], Seq[A])], suffix: Seq[A]) {
+
+  /** Transforms each values of this witness by the function. */
+  def map[B](f: A => B): Witness[B] =
+    Witness(pumps.map { case (pre, pump) => (pre.map(f), pump.map(f)) }, suffix.map(f))
+
+  /** Constructs a witness strings.
+    *
+    * This result's `n`-th element means `n` times repeated sequence.
+    */
+  def toLazyList: LazyList[Seq[A]] =
+    LazyList
+      .from(0)
+      .map(n => pumps.flatMap { case (pre, pump) => pre ++ Seq.fill(n)(pump).flatten } ++ suffix)
+}
---
+++ modules/redos-core/shared/src/test/scala/codes/quine/labo/redos/WitnessSuite.scala
@@ -0,0 +1,21 @@
+package codes.quine.labo.redos
+
+class WitnessSuite extends munit.FunSuite {
+  test("Witness#map") {
+    assertEquals(
+      Witness(Seq((Seq(1), Seq(2))), Seq(3)).map(_ + 1),
+      Witness(Seq((Seq(2), Seq(3))), Seq(4))
+    )
+  }
+
+  test("Witness#toLazyList") {
+    assertEquals(
+      Witness(Seq((Seq(1), Seq(2))), Seq(3)).toLazyList.take(3),
+      LazyList(
+        Seq(1, 3),
+        Seq(1, 2, 3),
+        Seq(1, 2, 2, 3)
+      )
+    )
+  }
+}
