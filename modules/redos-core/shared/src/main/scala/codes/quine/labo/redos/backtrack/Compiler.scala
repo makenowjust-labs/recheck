a3e34095f168ce774d123e996eb6e72f97905ea8
TSUYUSATO Kitsune <make.just.on@gmail.com>
2020-11-07 19:37:28 +0900

    feat: add backtrack Compiler

--- modules/redos-core/shared/src/main/scala/codes/quine/labo/redos/backtrack/Compiler.scala
+++ modules/redos-core/shared/src/main/scala/codes/quine/labo/redos/backtrack/Compiler.scala
@@ -5,12 +5,202 @@ import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
 
+import codes.quine.labo.redos.backtrack.IR.CapBegin
+
+import data.IChar
+import data.UChar
 import regexp.Pattern
 import regexp.Pattern._
 import util.TryUtil
 
 /** Compiler from RegExp pattern to VM IR. */
 object Compiler {
+
+  def compile(pattern: Pattern): Try[IR] =
+    for {
+      _ <- Try(()) // Ensures a `Try` context surely.
+      capsSize = Compiler.capsSize(pattern)
+      names <- Compiler.names(pattern)
+      codes <- {
+        val FlagSet(_, ignoreCase, multiline, dotAll, unicode, _) = pattern.flagSet
+
+        def loop(node: Node, forward: Boolean): Try[State] = node match {
+          case Disjunction(ns) =>
+            TryUtil.traverse(ns)(loop(_, forward)).map(_.reduceRight(_.union(_)))
+          case Sequence(ns) =>
+            TryUtil.traverse(if (forward) ns else ns.reverse)(loop(_, forward)).map { ss =>
+              ss.foldLeft(State(IndexedSeq.empty, false))(_.concat(_))
+            }
+          case Capture(index, n) =>
+            loop(n, forward).map(State.capture(index, _, forward))
+          case NamedCapture(index, _, n) =>
+            loop(n, forward).map(State.capture(index, _, forward))
+          case Group(n) => loop(n, forward)
+          case Star(nonGreedy, n) =>
+            loop(n, forward).map(State.many(nonGreedy, _))
+          case Plus(nonGreedy, n) =>
+            loop(n, forward).map(State.some(nonGreedy, _))
+          case Question(nonGreedy, n) =>
+            loop(n, forward).map(State.some(nonGreedy, _))
+          case Repeat(_, min, None, n) =>
+            loop(n, forward).map(State.repeatN(min, _))
+          case Repeat(nonGreedy, min, Some(None), n) =>
+            loop(n, forward).map(s => State.repeatN(min, s).concat(State.many(nonGreedy, s)))
+          case Repeat(_, min, Some(Some(max)), _) if min > max =>
+            Failure(new InvalidRegExpException("out of order repetition quantifier"))
+          case Repeat(nonGreedy, min, Some(Some(max)), n) =>
+            loop(n, forward).map(s => State.repeatN(min, s).concat(State.repeatAtMost(max - min, nonGreedy, s)))
+          case WordBoundary(invert) =>
+            Success(State(IndexedSeq(if (invert) IR.WordBoundaryNot else IR.WordBoundary), false))
+          case LineBegin =>
+            Success(State(IndexedSeq(if (multiline) IR.LineBegin else IR.InputBegin), false))
+          case LineEnd =>
+            Success(State(IndexedSeq(if (multiline) IR.LineEnd else IR.InputEnd), false))
+          case LookAhead(negative, n) =>
+            loop(n, true).map(State.lookAround(negative, _))
+          case LookBehind(negative, n) =>
+            loop(n, false).map(State.lookAround(negative, _))
+          case Character(c0) =>
+            val c = if (ignoreCase) UChar.canonicalize(c0, unicode) else c0
+            Success(State.char(IR.Char(c), forward))
+          case node @ CharacterClass(invert, _) =>
+            node.toIChar(ignoreCase, unicode).map { ch0 =>
+              val ch = if (ignoreCase) IChar.canonicalize(ch0, unicode) else ch0
+              State.char(if (invert) IR.ClassNot(ch) else IR.Class(ch), forward)
+            }
+          case node: AtomNode =>
+            node.toIChar(ignoreCase, unicode).map { ch0 =>
+              val ch = if (ignoreCase) IChar.canonicalize(ch0, unicode) else ch0
+              State.char(IR.Class(ch), forward)
+            }
+          case Dot =>
+            Success(State.char(if (dotAll) IR.Any else IR.Dot, forward))
+          case BackReference(i) =>
+            if (i <= 0 || capsSize < i) Failure(new InvalidRegExpException("invalid back-reference"))
+            else Success(State(IndexedSeq(if (forward) IR.Ref(i) else IR.RefBack(i)), false))
+          case NamedBackReference(name) =>
+            names.get(name) match {
+              case Some(i) =>
+                if (i <= 0 || capsSize < i) Failure(new InvalidRegExpException("invalid named back-reference"))
+                else Success(State(IndexedSeq(if (forward) IR.Ref(i) else IR.RefBack(i)), false))
+              case None => Failure(new InvalidRegExpException("invalid named back-reference"))
+            }
+        }
+        loop(pattern.node, true).map(State.prelude(pattern.hasLineBeginAtBegin, _))
+      }
+    } yield IR(pattern, capsSize, names, codes)
+
+  private[backtrack] final case class State(codes: IndexedSeq[IR.OpCode], advance: Boolean) {
+    def union(that: State): State =
+      State(
+        IndexedSeq(IR.ForkCont(codes.size + 1)) ++ codes ++ IndexedSeq(IR.Jump(that.codes.size)) ++ codes,
+        advance && this.advance
+      )
+
+    def concat(that: State): State =
+      State(codes ++ that.codes, advance || that.advance)
+
+    def captureRange: Option[(Int, Int)] = {
+      val captures = codes.collect {
+        case IR.CapBegin(i) => i
+        case IR.CapEnd(i)   => i
+      }
+      if (captures.nonEmpty) Some((captures.min, captures.max))
+      else None
+    }
+  }
+
+  private[backtrack] object State {
+    def prelude(hasLineBeginAtBegin: Boolean, state: State): IndexedSeq[IR.OpCode] = {
+      val begin =
+        if (!hasLineBeginAtBegin) IndexedSeq(IR.ForkNext(2), IR.Any, IR.Jump(-3), IR.CapBegin(0))
+        else IndexedSeq(IR.CapBegin(0))
+      val end = IndexedSeq(IR.CapEnd(0), IR.Done)
+      begin ++ state.codes ++ end
+    }
+
+    def capture(index: Int, state: State, forward: Boolean): State =
+      State(
+        if (forward) IndexedSeq(IR.CapBegin(index)) ++ state.codes ++ IndexedSeq(IR.CapEnd(index))
+        else IndexedSeq(IR.CapEnd(index)) ++ state.codes ++ IndexedSeq(CapBegin(index)),
+        state.advance
+      )
+
+    def many(nonGreedy: Boolean, state: State): State = {
+      val codes = setupLoop(state)
+      State(
+        IndexedSeq(if (nonGreedy) IR.ForkNext(codes.size + 1) else IR.ForkCont(codes.size + 1)) ++
+          codes ++
+          IndexedSeq(IR.Jump(-1 - codes.size - 1)),
+        false
+      )
+    }
+
+    def some(nonGreedy: Boolean, state: State): State = {
+      val codes = setupLoop(state)
+      State(
+        state.codes ++
+          IndexedSeq(if (nonGreedy) IR.ForkNext(codes.size + 1) else IR.ForkCont(codes.size + 1)) ++
+          codes ++
+          IndexedSeq(IR.Jump(-1 - codes.size - 1)),
+        state.advance
+      )
+    }
+
+    def optional(nonGreedy: Boolean, state: State): State =
+      State(
+        IndexedSeq(if (nonGreedy) IR.ForkNext(state.codes.size) else IR.ForkCont(state.codes.size)) ++ state.codes,
+        false
+      )
+
+    def repeatN(n: Int, state: State): State = n match {
+      case 0 => State(IndexedSeq.empty, false)
+      case 1 => state
+      case _ =>
+        State(
+          IndexedSeq(IR.Push(n)) ++ state.codes ++ IndexedSeq(IR.Dec, IR.Loop(-1 - state.codes.size - 1), IR.Pop),
+          state.advance
+        )
+    }
+
+    def repeatAtMost(n: Int, nonGreedy: Boolean, state: State): State = n match {
+      case 0 => State(IndexedSeq.empty, false)
+      case 1 => optional(nonGreedy, state)
+      case n =>
+        val codes = setupLoop(state)
+        State(
+          IndexedSeq(IR.Push(n), if (nonGreedy) IR.ForkNext(codes.size + 3) else IR.ForkCont(codes.size + 3)) ++
+            codes ++
+            IndexedSeq(IR.Dec, IR.Loop(-1 - codes.size - 2), IR.Pop),
+          false
+        )
+    }
+
+    def setupLoop(state: State): IndexedSeq[IR.OpCode] = {
+      val codes = if (state.advance) state.codes else IndexedSeq(IR.PushPos) ++ state.codes ++ IndexedSeq(IR.EmptyCheck)
+      state.captureRange match {
+        case Some((min, max)) => IndexedSeq(IR.CapReset(min, max)) ++ codes
+        case None             => codes
+      }
+    }
+
+    def lookAround(negative: Boolean, state: State): State = {
+      State(
+        if (negative)
+          IndexedSeq(IR.PushPos, IR.PushProc, IR.ForkCont(state.codes.size + 2)) ++ state.codes ++ IndexedSeq(
+            IR.RewindProc,
+            IR.Fail,
+            IR.Pop,
+            IR.RestorePos
+          )
+        else IndexedSeq(IR.PushPos, IR.PushProc) ++ state.codes ++ IndexedSeq(IR.RewindProc, IR.RestorePos),
+        false
+      )
+    }
+
+    def char(code: IR.OpCode, forward: Boolean): State =
+      State(if (forward) IndexedSeq(code) else IndexedSeq(IR.Back, code, IR.Back), true)
+  }
 
   /** Computes a size of captures of the pattern. */
   private[backtrack] def capsSize(pattern: Pattern): Int = {
