5bc83e5a44b22d46d5ba656f1c1b21834237e624
TSUYUSATO Kitsune <make.just.on@gmail.com>
2020-11-09 00:22:19 +0900

    feat: add backtrack Engine

---
+++ modules/redos-core/shared/src/main/scala/codes/quine/labo/redos/backtrack/Engine.scala
@@ -0,0 +1,15 @@
+package codes.quine.labo.redos
+package backtrack
+
+import scala.util.Try
+
+import data.UString
+import regexp.Pattern
+
+/** RegExp pattern matching Engine (frontend). */
+object Engine {
+
+  /** Tests a matching of the pattern on the input. */
+  def matches(pattern: Pattern, input: String, pos: Int = 0): Try[Option[Match]] =
+    IRCompiler.compile(pattern).map(VM.execute(_, UString.from(input, pattern.flagSet.unicode), pos))
+}
