e537bf8644652602ef043cc76a63f6e7cf977e10
TSUYUSATO Kitsune <make.just.on@gmail.com>
2020-11-19 21:39:16 +0900

    fix: wrap ReDoS.check in Try context correctly

--- modules/redos-core/shared/src/main/scala/codes/quine/labo/redos/ReDoS.scala
+++ modules/redos-core/shared/src/main/scala/codes/quine/labo/redos/ReDoS.scala
@@ -1,5 +1,7 @@
 package codes.quine.labo.redos
 
+import scala.util.Try
+
 import regexp.Parser
 
 /** ReDoS is a ReDoS checker frontend. */
@@ -8,12 +10,11 @@ object ReDoS {
   /** Tests the given RegExp pattern causes ReDoS. */
   def check(source: String, flags: String, config: Config = Config()): Diagnostics = {
     import config._
-    Parser
-      .parse(source, flags)
-      .flatMap(checker.check(_, config))
-      .recover { case ex: ReDoSException =>
-        Diagnostics.Unknown.from(ex)
-      }
-      .get
+    val result = for {
+      _ <- Try(()) // Ensures `Try` context.
+      pattern <- Parser.parse(source, flags)
+      diagnostics <- checker.check(pattern, config)
+    } yield diagnostics
+    result.recover { case ex: ReDoSException => Diagnostics.Unknown.from(ex) }.get
   }
 }
--- modules/redos-core/shared/src/test/scala/codes/quine/labo/redos/ReDoSSuite.scala
+++ modules/redos-core/shared/src/test/scala/codes/quine/labo/redos/ReDoSSuite.scala
@@ -1,9 +1,17 @@
 package codes.quine.labo.redos
+
+import scala.concurrent.duration._
+
 import automaton.Complexity
+import util.Timeout
 
 class ReDoSSuite extends munit.FunSuite {
   test("ReDoS.check") {
     assertEquals(ReDoS.check("^foo$", ""), Diagnostics.Safe(Some(Complexity.Constant)))
     assertEquals(ReDoS.check("", "x"), Diagnostics.Unknown(Diagnostics.ErrorKind.InvalidRegExp("unknown flag")))
+    assertEquals(
+      ReDoS.check("^foo$", "", Config(timeout = Timeout.from(0.second))),
+      Diagnostics.Unknown(Diagnostics.ErrorKind.Timeout)
+    )
   }
 }
