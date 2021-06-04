e29dad4e9c47be827b9c5cd1b62bea81e53dadc4
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-06-04 11:57:13 +0900

    Add comments to BatchCommand

--- modules/recheck-cli/src/main/scala/codes/quine/labo/recheck/cli/BatchCommand.scala
+++ modules/recheck-cli/src/main/scala/codes/quine/labo/recheck/cli/BatchCommand.scala
@@ -13,13 +13,17 @@ import codes.quine.labo.recheck.cli.BatchCommand._
 import codes.quine.labo.recheck.cli.codecs._
 import codes.quine.labo.recheck.diagnostics.Diagnostics
 
+/** `recheck batch` method types. */
 object BatchCommand {
+
+  /** `"check"` method parameter. */
   final case class CheckParams(source: String, flags: String, config: InputConfig)
 
   object CheckParams {
     implicit def decode: Decoder[CheckParams] = deriveDecoder
   }
 
+  /** `"cancel"` method parameter. */
   final case class CancelParams(id: RPC.ID)
 
   object CancelParams {
@@ -27,19 +31,28 @@ object BatchCommand {
   }
 }
 
+/** `recheck batch` command implementation. */
 class BatchCommand(threadSize: Int) {
+
+  /** A thread pool used by RPC runner. */
   val executor: ExecutorService = Executors.newFixedThreadPool(threadSize)
+
+  /** An execution context wrapping the thread pool. */
   implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(executor)
 
+  /** A map from current running request IDs to cancel functions. */
   def cancels: mutable.Map[RPC.ID, () => Unit] = mutable.Map.empty
 
+  /** `"check"` method implementation. */
   def handleCheck(id: RPC.ID, params: CheckParams): Either[RPC.Error, Diagnostics] = {
     val (config, cancel) = params.config.instantiate()
+    cancels.remove(id).foreach(_.apply()) // Cancel before
     cancels.update(id, cancel)
     try Right(ReDoS.check(params.source, params.flags, config))
     finally cancels.remove(id)
   }
 
+  /** `"cancel"` method implementation. */
   def handleCancel(params: CancelParams): Unit = {
     cancels.get(params.id).foreach(_.apply())
   }
@@ -51,5 +64,9 @@ class BatchCommand(threadSize: Int) {
         "check" -> RPC.RequestHandler(handleCheck),
         "cancel" -> RPC.NotificationHandler(handleCancel)
       )
-    } finally executor.shutdown()
+    } finally {
+      cancels.foreach(_._2.apply())
+      cancels.clear()
+      executor.shutdown()
+    }
 }
