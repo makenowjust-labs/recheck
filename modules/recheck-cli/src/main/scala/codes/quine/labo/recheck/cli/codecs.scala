2d8fadf007261b81ebe3ebd002424a1cfc98b5d8
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-06-04 11:56:50 +0900

    Add tests for codecs

--- modules/recheck-cli/src/main/scala/codes/quine/labo/recheck/cli/codecs.scala
+++ modules/recheck-cli/src/main/scala/codes/quine/labo/recheck/cli/codecs.scala
@@ -1,6 +1,5 @@
 package codes.quine.labo.recheck.cli
 
-import cats.syntax.contravariant._
 import io.circe.Decoder
 import io.circe.Encoder
 import io.circe.Json
@@ -14,104 +13,90 @@ import codes.quine.labo.recheck.diagnostics.Diagnostics.ErrorKind
 import codes.quine.labo.recheck.diagnostics.Hotspot
 import codes.quine.labo.recheck.unicode.UString
 
+/** The object `codes` provides circe's `Encoder`/`Decoder` instances for this application. */
 object codecs {
+
+  /** An `Encoder` for `Diagnostics`. */
   implicit def encodeDiagnostics: Encoder[Diagnostics] = {
     case Diagnostics.Safe(source, flags, complexity, checker) =>
       Json.obj(
-        "source" -> source.asJson,
-        "flags" -> flags.asJson,
-        "status" -> "safe".asJson,
-        "checker" -> checker.asJson,
-        "complexity" -> complexity.asJson(encodeAttackComplexity.narrow)
+        "source" := source,
+        "flags" := flags,
+        "status" := "safe",
+        "checker" := checker,
+        "complexity" := complexity.asInstanceOf[AttackComplexity]
       )
     case Diagnostics.Vulnerable(source, flags, complexity, attack, hotspot, checker) =>
       Json.obj(
-        "source" -> source.asJson,
-        "flags" -> flags.asJson,
-        "status" -> "vulnerable".asJson,
-        "checker" -> checker.asJson,
-        "complexity" -> complexity.asJson(encodeAttackComplexity.narrow),
-        "attack" -> attack.asJson,
-        "hotspot" -> hotspot.asJson
+        "source" := source,
+        "flags" := flags,
+        "status" := "vulnerable",
+        "checker" := checker,
+        "complexity" := complexity.asInstanceOf[AttackComplexity],
+        "attack" := attack,
+        "hotspot" := hotspot
       )
     case Diagnostics.Unknown(source, flags, error, checker) =>
       Json.obj(
-        "source" -> source.asJson,
-        "flags" -> flags.asJson,
-        "status" -> "unknown".asJson,
-        "checker" -> checker.asJson,
-        "error" -> error.asJson
+        "source" := source,
+        "flags" := flags,
+        "status" := "unknown",
+        "checker" := checker,
+        "error" := error
       )
   }
 
+  /** An `Encoder` for `Checker`. */
   implicit def encodeChecker: Encoder[Checker] = {
     case Checker.Hybrid    => "hybrid".asJson
     case Checker.Automaton => "automaton".asJson
     case Checker.Fuzz      => "fuzz".asJson
   }
 
+  /** An `Encoder` for `AttackComplexity`. */
   implicit def encodeAttackComplexity: Encoder[AttackComplexity] = {
     case c @ AttackComplexity.Constant =>
-      Json.obj(
-        "type" -> "constant".asJson,
-        "summary" -> c.toString.asJson,
-        "isFuzz" -> false.asJson
-      )
+      Json.obj("type" := "constant", "summary" := c.toString, "isFuzz" := false)
     case c @ AttackComplexity.Linear =>
-      Json.obj(
-        "type" -> "linear".asJson,
-        "summary" -> c.toString.asJson,
-        "isFuzz" -> false.asJson
-      )
+      Json.obj("type" := "linear", "summary" := c.toString, "isFuzz" := false)
     case c: AttackComplexity.Safe =>
-      Json.obj(
-        "type" -> "safe".asJson,
-        "summary" -> c.toString.asJson,
-        "isFuzz" -> c.isFuzz.asJson
-      )
+      Json.obj("type" := "safe", "summary" := c.toString, "isFuzz" := c.isFuzz)
     case c: AttackComplexity.Polynomial =>
-      Json.obj(
-        "type" -> "polynomial".asJson,
-        "degree" -> c.degree.asJson,
-        "summary" -> c.toString.asJson,
-        "isFuzz" -> c.isFuzz.asJson
-      )
+      Json.obj("type" := "polynomial", "degree" := c.degree, "summary" := c.toString, "isFuzz" := c.isFuzz)
     case c: AttackComplexity.Exponential =>
-      Json.obj(
-        "type" -> "exponential".asJson,
-        "summary" -> c.toString.asJson,
-        "isFuzz" -> c.isFuzz.asJson
-      )
+      Json.obj("type" := "exponential", "summary" := c.toString, "isFuzz" := c.isFuzz)
   }
 
+  /** An `Encoder` for `AttackPattern`. */
   implicit def encodeAttackPattern: Encoder[AttackPattern] = (p: AttackPattern) =>
     Json.obj(
-      "pumps" ->
-        Json.arr(p.pumps.map { case (p, s, n) =>
-          Json.obj("prefix" -> p.asJson, "pump" -> s.asJson, "bias" -> n.asJson)
-        }: _*),
-      "suffix" -> p.suffix.asJson,
-      "base" -> p.n.asJson,
-      "string" -> p.asUString.asJson,
-      "pattern" -> p.toString.asJson
+      "pumps" := Json.arr(p.pumps.map { case (p, s, n) => Json.obj("prefix" := p, "pump" := s, "bias" := n) }: _*),
+      "suffix" := p.suffix,
+      "base" := p.n,
+      "string" := p.asUString,
+      "pattern" := p.toString
     )
 
+  /** An `Encoder` for `Hotspot`. */
   implicit def encodeHotspot: Encoder[Hotspot] = (h: Hotspot) =>
-    Json.arr(h.spots.map { case Hotspot.Spot(start, end, temperature) =>
-      Json.obj("start" -> start.asJson, "end" -> end.asJson, "temperature" -> temperature.toString.asJson)
+    Json.arr(h.spots.map { case Hotspot.Spot(s, e, t) =>
+      Json.obj("start" := s, "end" := e, "temperature" := t.toString)
     }: _*)
 
+  /** An `Encoder` for `ErrorKind`. */
   implicit def encodeErrorKind: Encoder[ErrorKind] = {
     case ErrorKind.Timeout =>
-      Json.obj("kind" -> "timeout".asJson)
+      Json.obj("kind" := "timeout")
     case ErrorKind.Unsupported(message) =>
-      Json.obj("kind" -> "unsupported".asJson, "message" -> message.asJson)
+      Json.obj("kind" := "unsupported", "message" := message)
     case ErrorKind.InvalidRegExp(message) =>
-      Json.obj("kind" -> "invalid".asJson, "message" -> message.asJson)
+      Json.obj("kind" := "invalid", "message" := message)
   }
 
+  /** An `Encoder` for `UString`. */
   implicit def encodeUString: Encoder[UString] = _.asString.asJson
 
+  /** A `Decoder` for `Checker`. */
   implicit def decodeChecker: Decoder[Checker] =
     Decoder[String].emap {
       case "hybrid"    => Right(Checker.Hybrid)
