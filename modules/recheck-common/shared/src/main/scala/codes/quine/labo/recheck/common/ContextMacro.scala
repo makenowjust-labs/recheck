7e1b2e2e13b2a6ec8a5362c454ddf32c58696f29
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-12-16 23:17:44 +0900

    Add logging API for context

--- modules/recheck-common/shared/src/main/scala/codes/quine/labo/recheck/common/ContextMacro.scala
+++ modules/recheck-common/shared/src/main/scala/codes/quine/labo/recheck/common/ContextMacro.scala
@@ -2,16 +2,26 @@ package codes.quine.labo.recheck.common
 
 import scala.reflect.macros.blackbox
 
+/** ContextMacro is an object holds [[Context]] method implementations. */
 private[common] object ContextMacro {
-  def interrupt(c: blackbox.Context)(body: c.Tree): c.Tree = {
-    import c.universe._
 
-    val ctx = c.prefix
+  /** Returns a string literal tree represents caller location. */
+  def sourceLiteral(c: blackbox.Context): c.Tree = {
+    import c.universe._
 
     val pos = c.enclosingPosition
     val path = pos.source.path.replaceFirst("\\A.*/(?=modules)", "")
     val source = Literal(Constant(s"$path:${pos.line}"))
 
+    source
+  }
+
+  /** [[Context#interrupt]] implementation. */
+  def interrupt(c: blackbox.Context)(body: c.Tree): c.Tree = {
+    import c.universe._
+
+    val ctx = c.prefix
+    val source = sourceLiteral(c)
     val isCancelled = q"$ctx.token != null && $ctx.token.isCancelled()"
     val isInterrupted = q"$isCancelled || $ctx.deadline != null && $ctx.deadline.isOverdue()"
 
@@ -21,6 +31,17 @@ private[common] object ContextMacro {
         else throw new codes.quine.labo.recheck.common.TimeoutException($source)
       }
       $body
+    """
+  }
+
+  /** [[Context#log]] implementation. */
+  def log(c: blackbox.Context)(message: c.Tree): c.Tree = {
+    import c.universe._
+
+    val ctx = c.prefix
+
+    q"""
+      if ($ctx.hasLogger) $ctx.logger($message)
     """
   }
 }
