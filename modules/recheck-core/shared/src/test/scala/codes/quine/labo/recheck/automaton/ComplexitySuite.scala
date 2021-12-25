0c99812d882336f9abad5ce68d9b7bd3b5ec4353
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-12-25 14:10:51 +0900

    Fix Complexity#buildAttackPattern test

--- modules/recheck-core/shared/src/test/scala/codes/quine/labo/recheck/automaton/ComplexitySuite.scala
+++ modules/recheck-core/shared/src/test/scala/codes/quine/labo/recheck/automaton/ComplexitySuite.scala
@@ -22,6 +22,6 @@ class ComplexitySuite extends munit.FunSuite {
   test("Complexity.Vulnerable#buildAttackPattern") {
     val w = Witness(Seq((Seq(UChar('a')), Seq(UChar('a')))), Seq(UChar('\u0000')))
     assertEquals(Exponential(w).buildAttackPattern(100_000, 4_000).asUString.sizeAsString, 19)
-    assertEquals(Polynomial(2, w).buildAttackPattern(100_000, 4_000).asUString.sizeAsString, 319)
+    assertEquals(Polynomial(2, w).buildAttackPattern(100_000, 4_000).asUString.sizeAsString, 450)
   }
 }
