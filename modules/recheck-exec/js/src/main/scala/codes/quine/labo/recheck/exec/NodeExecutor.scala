f3c4f435c035176fe9151f2329d7716f1972fbac
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-12-27 22:59:40 +0900

    Add $COVERAGE-OFF$ comment

--- modules/recheck-exec/js/src/main/scala/codes/quine/labo/recheck/exec/NodeExecutor.scala
+++ modules/recheck-exec/js/src/main/scala/codes/quine/labo/recheck/exec/NodeExecutor.scala
@@ -12,5 +12,7 @@ object NodeExecutor {
   /** Executes `node` command. */
   @nowarn
   def exec(code: String, timeout: Option[FiniteDuration])(implicit ctx: Context): Option[(Int, String, String)] =
+    // $COVERAGE-OFF$
     throw new UnexpectedException("recall validation is not supported.")
+  // $COVERAGE-ON$
 }
