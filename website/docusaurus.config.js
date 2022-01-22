429de5d6bee6dface97bfbee3dfc129b33cb4dca
TSUYUSATO Kitsune <make.just.on@gmail.com>
2022-01-22 23:38:23 +0900

    Apply prettier

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,110 +1,111 @@
 // @ts-check
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require("remark-math");
+const katex = require("rehype-katex");
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'recheck',
-  tagline: 'The trustworthy ReDoS checker',
-  url: 'https://makenowjust-labo.github.io',
-  baseUrl: '/recheck/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
-  organizationName: 'MakeNowJust-Labo',
-  projectName: 'recheck',
+  title: "recheck",
+  tagline: "The trustworthy ReDoS checker",
+  url: "https://makenowjust-labo.github.io",
+  baseUrl: "/recheck/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "MakeNowJust-Labo",
+  projectName: "recheck",
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/MakeNowJust-Labo/recheck/tree/main/website/docs',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl:
+            "https://github.com/MakeNowJust-Labo/recheck/tree/main/website/docs",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
-  plugins: ['@docusaurus/plugin-ideal-image'],
+  plugins: ["@docusaurus/plugin-ideal-image"],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Recheck',
+        title: "Recheck",
         logo: {
-          alt: 'Recheck Logo',
-          src: 'img/logo-light.svg',
-          srcDark: 'img/logo-dark.svg',
+          alt: "Recheck Logo",
+          src: "img/logo-light.svg",
+          srcDark: "img/logo-dark.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
           {
-            to: 'playground',
-            position: 'left',
-            label: 'Playground',
+            to: "playground",
+            position: "left",
+            label: "Playground",
           },
           {
-            href: 'https://github.com/MakeNowJust-Labo/recheck',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/MakeNowJust-Labo/recheck",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Introduction',
-                to: '/docs/intro',
+                label: "Introduction",
+                to: "/docs/intro",
               },
               {
-                label: 'Usage',
-                to: '/docs/category/usage',
+                label: "Usage",
+                to: "/docs/category/usage",
               },
               {
-                label: 'Internals',
-                to: '/docs/category/internals',
+                label: "Internals",
+                to: "/docs/category/internals",
               },
             ],
           },
           {
-            title: 'Try',
+            title: "Try",
             items: [
               {
-                label: 'Playground',
-                to: '/playground',
+                label: "Playground",
+                to: "/playground",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub Repository',
-                href: 'https://github.com/MakeNowJust-Labo/recheck',
+                label: "GitHub Repository",
+                href: "https://github.com/MakeNowJust-Labo/recheck",
               },
               {
-                label: '@MakeNowJust-Labo',
-                href: 'https://github.com/MakeNowJust-Labo',
+                label: "@MakeNowJust-Labo",
+                href: "https://github.com/MakeNowJust-Labo",
               },
             ],
           },
@@ -114,16 +115,16 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['java', 'scala'],
+        additionalLanguages: ["java", "scala"],
       },
     }),
 
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css",
       integrity:
-        'sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc',
-      crossorigin: 'anonymous',
+        "sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc",
+      crossorigin: "anonymous",
     },
   ],
 };
--- website/sidebars.js
+++ website/sidebars.js
@@ -3,36 +3,36 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   docs: [
-    'intro',
+    "intro",
     {
-      type: 'category',
-      label: 'Usage',
+      type: "category",
+      label: "Usage",
       link: {
-        type: 'generated-index',
+        type: "generated-index",
       },
       collapsed: false,
       items: [
-        'usage/as-javascript-library',
-        'usage/as-scala-library',
-        'usage/as-eslint-plugin',
-        'usage/parameters',
-        'usage/diagnostics'
+        "usage/as-javascript-library",
+        "usage/as-scala-library",
+        "usage/as-eslint-plugin",
+        "usage/parameters",
+        "usage/diagnostics",
       ],
     },
     {
-      type: 'category',
-      label: 'Internals',
+      type: "category",
+      label: "Internals",
       link: {
-        type: 'generated-index',
+        type: "generated-index",
       },
       items: [
-        'internals/background',
-        'internals/fuzzing',
-        'internals/matching-acceleration',
-        'internals/criteria',
-      ]
-    }
-  ]
+        "internals/background",
+        "internals/fuzzing",
+        "internals/matching-acceleration",
+        "internals/criteria",
+      ],
+    },
+  ],
 };
 
 module.exports = sidebars;
