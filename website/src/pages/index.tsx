c00952541f552d8e73e39e2b243b8ba60f22eca5
TSUYUSATO Kitsune <make.just.on@gmail.com>
2022-01-26 10:15:19 +0900

    Fix logo style

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,8 +1,10 @@
 import clsx from 'clsx';
 import React from 'react';
 
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import ThemedImage from '@theme/ThemedImage';
 import useThemeContext from '@theme/hooks/useThemeContext';
 
 import styles from './index.module.css';
@@ -12,12 +14,14 @@ import LogoDark from '../../static/img/logo-dark.svg';
 
 function HomeHeader() {
   const { siteConfig } = useDocusaurusContext();
-  const { isDarkTheme } = useThemeContext();
-  const Logo = isDarkTheme ? LogoDark : LogoLight;
+  const sources = {
+    light: useBaseUrl('/img/logo-light.svg'),
+    dark: useBaseUrl('/img/logo-dark.svg'),
+  };
   return (
     <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
-        <Logo className={styles.heroBannerLogo} />
+        <ThemedImage sources={sources} width={300} height={300} title={'recheck logo'} />
         <h1 className={clsx('hero__title', styles.heroBannerText)}>{siteConfig.title}</h1>
         <p className={clsx('hero__subtitle', styles.heroBannerText)}>{siteConfig.tagline}</p>
       </div>
