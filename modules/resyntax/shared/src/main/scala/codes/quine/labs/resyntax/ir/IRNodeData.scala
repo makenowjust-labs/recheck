3a86896df7a59536aa8ecfa4f3dd8b26ca45ee5f
TSUYUSATO Kitsune <make.just.on@gmail.com>
2022-06-05 21:59:59 +0900

    Add IR (WIP)

---
+++ modules/resyntax/shared/src/main/scala/codes/quine/labs/resyntax/ir/IRNodeData.scala
@@ -0,0 +1,15 @@
+package codes.quine.labs.resyntax.ir
+
+import codes.quine.labs.resyntax.ast.NodeData
+
+/** IRNodeData is a data of internal representation node. */
+sealed abstract class IRNodeData extends Product with Serializable
+
+object IRNodeData {
+
+  /** Assert is a simple zero-width assertion. */
+  final case class Assert(kind: IRAssertKind) extends IRNodeData
+
+  /** Unsupported is a wrapper of AST node. */
+  final case class Unsupported(data: NodeData) extends IRNodeData
+}
