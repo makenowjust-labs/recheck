073763134b38e5d91980e2e3b97e1ff562abc15c
TSUYUSATO Kitsune <make.just.on@gmail.com>
2022-06-24 13:54:53 +0900

    Add test for Group

--- modules/resyntax/shared/src/test/scala/codes/quine/labs/resyntax/ir/IRBuilderSuite.scala
+++ modules/resyntax/shared/src/test/scala/codes/quine/labs/resyntax/ir/IRBuilderSuite.scala
@@ -1,8 +1,12 @@
 package codes.quine.labs.resyntax.ir
 
+import codes.quine.labs.resyntax.ast.AssertNameStyle
 import codes.quine.labs.resyntax.ast.Dialect
 import codes.quine.labs.resyntax.ast.Dialect._
 import codes.quine.labs.resyntax.ast.FlagSet
+import codes.quine.labs.resyntax.ast.GroupKind
+import codes.quine.labs.resyntax.ast.NameStyle
+import codes.quine.labs.resyntax.ast.NodeData
 import codes.quine.labs.resyntax.ir.IRNodeData._
 import codes.quine.labs.resyntax.parser.Parser
 
@@ -21,12 +25,30 @@ class IRBuilderSuite extends munit.FunSuite {
   val All: Seq[Dialect] = Seq(DotNet, Java, JavaScript, PCRE, Perl, Python, Ruby)
 
   // Group
+  check("(?<-x>)", "", DotNet)(
+    Unsupported(NodeData.Group(GroupKind.Balance(NameStyle.Angle, None, "x"), NodeData.Sequence()))
+  )
+  check("(?>)", "", DotNet, Java, PCRE, Perl, Ruby)(
+    Unsupported(NodeData.Group(GroupKind.Atomic(AssertNameStyle.Symbolic), NodeData.Sequence()))
+  )
+  check("(?*)", "", PCRE)(
+    Unsupported(NodeData.Group(GroupKind.NonAtomicPositiveLookAhead(AssertNameStyle.Symbolic), NodeData.Sequence()))
+  )
+  check("(?<*)", "", PCRE)(
+    Unsupported(NodeData.Group(GroupKind.NonAtomicPositiveLookBehind(AssertNameStyle.Symbolic), NodeData.Sequence()))
+  )
+  check("(*script_run:)", "", PCRE, Perl)(
+    Unsupported(NodeData.Group(GroupKind.ScriptRun(AssertNameStyle.Alphabetic), NodeData.Sequence()))
+  )
+  check("(*atomic_script_run:)", "", PCRE, Perl)(
+    Unsupported(NodeData.Group(GroupKind.AtomicScriptRun(AssertNameStyle.Alphabetic), NodeData.Sequence()))
+  )
   check("(?m:^)", "", DotNet, Java, PCRE, Perl, Python, Ruby)(Assert(IRAssertKind.LineBegin))
   check("(?-m:^)", "m", DotNet, Java, PCRE, Perl, Python)(Assert(IRAssertKind.TextBegin))
   check("(?-m:^)", "m", Ruby)(Assert(IRAssertKind.LineBegin))
-  check("(?m:$)", "", DotNet, Java, PCRE, Perl, Python, Ruby)(Assert(IRAssertKind.LineEnd))
-  check("(?-m:$)", "m", DotNet, Java, PCRE, Perl, Python)(Assert(IRAssertKind.ChompTextEnd))
-  check("(?-m:$)", "m", Ruby)(Assert(IRAssertKind.LineEnd))
+  check("(?^m:^)", "", PCRE, Perl)(Assert(IRAssertKind.LineBegin))
+  check("(?^:^)", "m", PCRE, Perl)(Assert(IRAssertKind.TextBegin))
+  check("(?~)", "", Ruby)(Unsupported(NodeData.Group(GroupKind.Absence, NodeData.Sequence())))
 
   // Caret
   check("^", "", DotNet, Java, JavaScript, PCRE, Perl, Python)(Assert(IRAssertKind.TextBegin))
